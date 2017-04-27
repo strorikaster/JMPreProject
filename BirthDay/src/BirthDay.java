8cdbd2a91e288ee72f21a4e9b9a34414204ab5bc
strorikaster <strorikaster@gmail.com>
2017-04-27 12:04:35 +0300

    BirthDay, TelegramExperiments

--- BirthDay/src/BirthDay.java
+++ BirthDay/src/BirthDay.java
@@ -1,8 +1,8 @@
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.Locale;
 import java.util.Date;
+import java.util.Locale;
 
 
 /**
@@ -10,20 +10,18 @@ import java.util.Date;
  */
 public class BirthDay {
     public static void main(String[] args) {
-        SimpleDateFormat needDateFormat = new SimpleDateFormat("dd-mm-yyyy - EEE", Locale.ENGLISH);
-        Calendar calendar = GregorianCalendar.getInstance();
-        calendar.set(1981, GregorianCalendar.MARCH, 30);
-        Date myBirthDay = calendar.getTime();
+        DateFormat needDateFormat = new SimpleDateFormat("dd.mm.yyyy - EEE", Locale.ENGLISH);
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(1981, Calendar.MARCH, 30);
         Date  currentDate = new Date();
+        Date myBirthDay = calendar.getTime();
         int i = 0;
 
-        while(currentDate.compareTo(myBirthDay) > 0 ){
+        while(currentDate.compareTo(myBirthDay) > 0 ) {
             System.out.println(i + " - " + needDateFormat.format(myBirthDay));
-            calendar.add(GregorianCalendar.YEAR, 1);
+            calendar.add(Calendar.YEAR, 1);
             myBirthDay = calendar.getTime();
             i++;
         }
-
-
     }
 }
