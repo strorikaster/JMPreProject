e92ef7ee71a61b00cfcd739e87d55d65603d7ae4
strorikaster <strorikaster@gmail.com>
2017-05-05 14:49:39 +0300

    ChessDeck3

--- Arrays/src/ArrayRevertPrint.java
+++ Arrays/src/ArrayRevertPrint.java
@@ -1,3 +1,6 @@
+import static java.util.Arrays.fill;
+import static java.util.Arrays.sort;
+
 /**
  * Created by A.Zotov on 03.05.2017.
  */
@@ -7,8 +10,21 @@ public class ArrayRevertPrint
     {
         String array[] = {"Red", "Orange", "Yellow", "Green", "Cyan", "Blue", "Violent"};
 
+        //reverse output of array
         for(int i = array.length-1; i >= 0; i--){
             System.out.println(array[i]);
         }
+        //sort(array, 0, 7);
+        if (array.length % 2 != 0){
+            array[0] = array[6];
+            array[1] = array[5];
+            array[2] = array[4];
+         }
+        for(String item : array){
+            System.out.println(item);
+        }
+
     }
+
+
 }
--- ChessDeck/src/ChessDeck.java
+++ ChessDeck/src/ChessDeck.java
@@ -5,16 +5,16 @@ public class ChessDeck
 {
     public static void main(String[] args)
     {
-        String[][] chessDeck =  {
-                {"a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"},
-                {"a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"},
-                {"a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3"},
-                {"a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4"},
-                {"a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5"},
-                {"a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6"},
-                {"a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"},
-                {"a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"}
-        };
+
+
+        String chessDeck[][] = new String[8][8];
+        String chessDeckHorizontal[] = {"a", "b", "c", "d", "e", "f", "g", "h"};
+        for (int j = 0; j < chessDeckHorizontal.length; j++){
+            for (int i = 0; i < chessDeckHorizontal.length; i++){
+               chessDeck[j][i] = chessDeckHorizontal[i] + (j+1);
+            }
+            //System.out.println();
+        }
 
         for (int j = 0; j < chessDeck.length; j++){
             for (int i = 0; i < chessDeck[j].length; i++){
